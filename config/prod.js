a204723d6395a71522bbd930f7c6daad429f1e55
ioncreature <comrade.muchacho@gmail.com>
2014-07-22 19:02:18 +0400

    Added initd script and prod config

---
+++ config/prod.js
@@ -0,0 +1,16 @@
+/**
+ * @author Alexander Marenin
+ * @date July 2014
+ */
+
+exports.debug = true;
+
+exports.port = 80;
+
+exports.mongodb = [
+    {
+        host: 'localhost',
+        port: 27017,
+        dbname: 'cardsProd'
+    }
+];
